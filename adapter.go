526c80dec850bc50f1d395670f7838f48e65f721
daqingshu <12083415+daqingshu@users.noreply.github.com>
2021-12-05 21:50:17 +0800

    expand v0 v1 size

--- adapter.go
+++ adapter.go
@@ -43,8 +43,8 @@ type customTableKey struct{}
 type CasbinRule struct {
 	ID    uint   `gorm:"primaryKey;autoIncrement"`
 	Ptype string `gorm:"size:100"`
-	V0    string `gorm:"size:100"`
-	V1    string `gorm:"size:100"`
+	V0    string `gorm:"size:200"`
+	V1    string `gorm:"size:200"`
 	V2    string `gorm:"size:100"`
 	V3    string `gorm:"size:100"`
 	V4    string `gorm:"size:100"`
